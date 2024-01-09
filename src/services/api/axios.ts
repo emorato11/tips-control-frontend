3eb68fa1f8a2d5463b622a0d1f4be41138834e20
Enrique Morato <quiquemorato@gmail.com>
2024-01-09 22:24:36 +0100

    include new endpoint for vercel API

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiInstance = axios.create({
-  baseURL: 'https://tips-control-backend.onrender.com/',
+  // baseURL: 'https://tips-control-backend.onrender.com/',
+  baseURL: 'https://tips-control-backend-vercel.vercel.app/',
   // baseURL: 'http://localhost:8080/',
   headers: {
     'Access-Control-Allow-Origin': '*',
