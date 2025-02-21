b41bf4dd41682b64a6833467dbb7fd9f645c7839
Enrique Morato <quiquemorato@gmail.com>
2025-02-21 13:11:27 +0100

    configure eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,81 +1,21 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
+  env: { browser: true, es2021: true },
   extends: [
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript/recommended',
-    '@vue/eslint-config-prettier',
-    'plugin:import/typescript'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended' // <- Integración con Prettier
   ],
-  env: {
-    'vue/setup-compiler-macros': true
-  },
+  parserOptions: { ecmaVersion: 2021, parser: '@typescript-eslint/parser', sourceType: 'module' },
   rules: {
-    'vue/html-self-closing': [
-      'error',
-      {
-        html: {
-          void: 'always',
-          normal: 'always'
-        }
-      }
-    ],
-    'vue/attribute-hyphenation': [
-      2,
-      'never',
-      {
-        ignore: []
-      }
-    ],
-    'vue/custom-event-name-casing': [
-      2,
-      'camelCase',
-      {
-        ignores: []
-      }
-    ],
-    'vue/v-on-event-hyphenation': [
-      'error',
-      'never',
-      {
-        autofix: true,
-        ignore: []
-      }
-    ],
-    'vue/prop-name-casing': [2, 'camelCase'],
-    'vue/component-options-name-casing': ['error', 'PascalCase'],
-    'vue/component-name-in-template-casing': [
-      'error',
-      'PascalCase',
-      {
-        registeredComponentsOnly: true,
-        ignores: []
-      }
-    ],
-    'vue/no-mutating-props': 1,
-    'vue/multi-word-component-names': 0,
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 100
-      },
-      {
-        usePrettierrc: true
-      }
-    ]
-  },
-  overrides: [
-    // **.ts
-    {
-      files: ['*.ts'],
-      extends: ['plugin:import/typescript'],
-      rules: {
-        '@typescript-eslint/consistent-type-imports': 'error'
-      }
-    }
-  ],
-  ignorePatterns: ['postcss.config.js']
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'vue/html-self-closing': 'warn',
+    'vue/attribute-hyphenation': ['error', 'always'],
+    'vue/html-indent': ['error', 2],
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 }
