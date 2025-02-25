2013e682075f2044fe1154463b725899300090cd
Enrique Morato <quiquemorato@gmail.com>
2025-02-25 21:18:37 +0100

    fix: try fix cors

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -4,10 +4,8 @@ const apiInstance = axios.create({
   // baseURL: 'https://tips-control-backend.onrender.com/',
   baseURL: 'https://tips-control-backend-vercel.vercel.app/',
   // baseURL: 'http://localhost:8080/',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Content-Type': 'application/json'
-  }
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true
 })
 
 apiInstance.interceptors.request.use(
